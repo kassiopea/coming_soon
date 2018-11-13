71a325dede82d78ebcffa6202510fe4b8bbc7b24
Anastasia <fotograf.anastasia@gmail.com>
2018-11-13 21:31:25 +0300

    перенесено объявление всех глобальных переменных в главный файл main

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,16 @@
 "use strict";
+var body = document.querySelector(".body-style-bg");
+var headerLogo = document.querySelector(".header__logo");
+var mainHeader = document.querySelector(".main-header");
+var mainHeaderContinius = document.querySelector(".main-header__continius");
+var description = document.querySelector(".main-header__description");
+var socialLinksList = document.querySelector(".social-links-list");
+var footerCopyright = document.querySelector(".footer__copyright");
 
-console.log("main.js");
+var wrapImg = document.querySelector(".wrap-img-bg");
+var imgItems = document.querySelectorAll (".wrap-img-bg-item");
+
+var windowWidth = window.innerWidth;
+
+var time = 1000
+var timeOut = 300;
